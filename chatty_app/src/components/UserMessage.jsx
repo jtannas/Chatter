9cd559a6e074e8f33b2a7b821533fbf578347d83
Joel Tannas <jtannas@gmail.com>
2018-02-21 13:46:09 -0700

    add image rendering to the chat

--- chatty_app/src/components/UserMessage.jsx
+++ chatty_app/src/components/UserMessage.jsx
@@ -2,10 +2,21 @@ import React, {Component} from 'react';
 
 class UserMessage extends Component {
   render() {
+    let content = this.props.message.content;
+    const images = content.trim().match(/\b\S+\.(?:gif|jpg|png)\b/gi);
+    if (images) {
+      content = (
+        <React.Fragment>
+          {this.props.message.content}
+          <hr/>
+          {images.map((image, index) => <img key={index} src={image}/>)}
+        </React.Fragment>
+      )
+    }
     return (
       <div className="message">
         <span className="message-username" style={{color: this.props.message.color}}>{this.props.message.username}</span>
-        <span className="message-content">{this.props.message.content}</span>
+        <span className="message-content">{content}</span>
       </div>
     );
   }
